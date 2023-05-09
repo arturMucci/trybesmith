b89b61c36d2243fca6014730678e86fa6a2e34dc
arturMucci <artur.mucci@gmail.com>
2023-05-09 13:00:43 -0300

    ProjetoTrybeSmith - milestone 05

--- src/middlewares/validateLogin.ts
+++ src/middlewares/validateLogin.ts
@@ -3,10 +3,7 @@ import createError from 'http-errors';
 import loginSchema from '../Joi/loginSchema';
 
 export default function validateLogin(req: Request, _res: Response, next: NextFunction) {
-  console.log(req.body);
-
   const { error } = loginSchema.validate(req.body);
-  console.log(error);
 
   if (error) {
     return next(createError(400, error.message));
